2177a8fdc32dec63712c8df8816202c9145ee568
Hello1Robot <109326426+Hello1Robot@users.noreply.github.com>
2022-08-16 09:19:15 +0900

    [Silver IV] Title: 큐, Time: 100 ms, Memory: 32460 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,31 @@
+import sys
+from collections import deque
+N = int(input())
+rst = []
+que = deque(rst)
+for _ in range(N):
+    A = sys.stdin.readline().split()
+    if A[0] == 'push':
+        que.append(A[1])
+    elif A[0] == 'front':
+        if que:
+            print(que[0])
+        else:
+            print(-1)
+    elif A[0] == 'back':
+        if que:
+            print(que[-1])
+        else:
+            print(-1)
+    elif A[0] == 'pop':
+        if que:
+            print(que.popleft())
+        else:
+            print(-1)
+    elif A[0] == 'size':
+        print(len(que))
+    elif A[0] == 'empty':
+        if len(que):
+            print(0)
+        else:
+            print(1)
