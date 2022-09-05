00704d40116752927f52de10090fc4bdd52ef3c1
Hello1Robot <109326426+Hello1Robot@users.noreply.github.com>
2022-09-05 23:32:58 +0900

    [Silver IV] Title: 큐 2, Time: 1924 ms, Memory: 171424 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -0,0 +1,30 @@
+import sys
+from collections import deque
+N = int(sys.stdin.readline())
+que = deque()
+for i in range(N):
+    cmd = sys.stdin.readline().strip().split()
+    if cmd[0] == 'push':
+        que.append(cmd[1])
+    elif cmd[0] == 'pop':
+        if que:
+            print(que.popleft())
+        else:
+            print(-1)
+    elif cmd[0] == 'size':
+        print(len(que))
+    elif cmd[0] == 'empty':
+        if que:
+            print(0)
+        else:
+            print(1)
+    elif cmd[0] == 'front':
+        if que:
+            print(que[0])
+        else:
+            print(-1)
+    elif cmd[0] == 'back':
+        if que:
+            print(que[-1])
+        else:
+            print(-1)
