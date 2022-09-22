ccb0af4f761a2f2ef4afe0bd236da220930aa919
Kwonmin Choi <109326426+Hello1Robot@users.noreply.github.com>
2022-09-22 17:52:44 +0900

    [Silver I] Title: 회의실 배정, Time: 424 ms, Memory: 49124 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,17 +1,19 @@
 import heapq
+import sys
+input = sys.stdin.readline
 
 N = int(input())
-화물차 = []
+회의실 = []
 for _ in range(N):
     시작, 끝 = map(int,input().split())
-    heapq.heappush(화물차, (끝, 시작))
+    heapq.heappush(회의실, (끝, 시작))
 cnt = 0
-while 화물차:
-    끝, 시작 = heapq.heappop(화물차)
+while 회의실:
+    끝, 시작 = heapq.heappop(회의실)
     cnt += 1
-    while 화물차:
-        다음끝, 다음시작 = heapq.heappop(화물차)
+    while 회의실:
+        다음끝, 다음시작 = heapq.heappop(회의실)
         if 다음시작 >= 끝:
-            heapq.heappush(화물차, (다음끝, 다음시작))
+            heapq.heappush(회의실, (다음끝, 다음시작))
             break
 print(cnt)
