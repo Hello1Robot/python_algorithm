091de39e89ea22ba4360887ae2bb9ee76238b47f
Hello1Robot <hello11robot@gmail.com>
2022-11-10 17:51:26 +0900

    가장 긴 감소하는 부분 수열 BOJ

---
+++ algorithm/11722_가장 긴 감소하는 부분 수열.py
@@ -0,0 +1,10 @@
+from sys import stdin; input=stdin.readline
+N = int(input())
+nums = tuple(map(int,input().split()))
+DP = [1] * N
+for i in range(1,N):
+    for j in range(i):
+        if nums[i] < nums[j]:
+            DP[i] = max(DP[i], DP[j]+1)
+
+print(max(DP))
