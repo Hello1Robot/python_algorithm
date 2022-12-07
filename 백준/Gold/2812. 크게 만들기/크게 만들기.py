faa04f64ab4a8a85ef59b25258c65a2d19418ae4
Kwonmin Choi <109326426+Hello1Robot@users.noreply.github.com>
2022-12-07 23:52:19 +0900

    [Gold III] Title: 크게 만들기, Time: 1780 ms, Memory: 37124 KB -BaekjoonHub

---
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.py
@@ -0,0 +1,28 @@
+from sys import stdin; input=stdin.readline
+N, K = map(int,input().split())
+nums = list(input().strip())
+stk = []
+for num in nums:
+    if K > 0:
+        if stk:
+            if stk[-1] >= num:
+                stk.append(num)
+            else:
+                while stk and stk[-1] < num:
+                    stk.pop()
+                    K -= 1
+                    if K == 0:
+                        break
+                stk.append(num)
+        else:
+            stk.append(num)
+
+    else:
+        stk.append(num)
+
+while K > 0:
+    stk.pop()
+    K -= 1
+
+print(int(''.join(stk)))
+
