8c102c325574db299d919070eef6a70e39f0a1f8
Kwonmin Choi <109326426+Hello1Robot@users.noreply.github.com>
2023-01-08 23:54:25 +0900

    [Gold III] Title: 사회망 서비스(SNS), Time: 4148 ms, Memory: 258584 KB -BaekjoonHub

---
+++ 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.py
@@ -0,0 +1,33 @@
+from sys import stdin; input=stdin.readline
+from collections import defaultdict
+N = int(input())
+
+nodes = defaultdict(list)
+visited = [0]*(N+1)
+conn = [0]*(N+1)
+leafs = []
+res = 0
+for _ in range(N-1):
+    stt, end = map(int,input().split())
+    nodes[stt].append(end)
+    nodes[end].append(stt)
+    conn[stt] += 1
+    conn[end] += 1
+
+
+
+for i in range(1,N+1):
+    if conn[i] == 1:
+        leafs.append(i)
+
+while leafs:
+    x = leafs.pop()
+    for i in nodes[x]:
+        for j in nodes[i]:
+            nodes[j].remove(i)
+            if len(nodes[j]) == 1:
+                leafs.append(j)
+        visited[i] = 1
+        res += 1
+        nodes[i].clear()
+print(res)
