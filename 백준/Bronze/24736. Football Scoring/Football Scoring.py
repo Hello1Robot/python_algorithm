d143bbe5a25ce7d7159ca0e1c15028e335b6854c
Kwonmin Choi <109326426+Hello1Robot@users.noreply.github.com>
2023-02-08 01:38:01 +0900

    [Bronze V] Title: Football Scoring, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/24736. Football Scoring/Football Scoring.py
@@ -0,0 +1,8 @@
+
+
+A = list(map(int,input().split()))
+B = list(map(int,input().split()))
+ra = A[0]*6 + A[1]*3 + A[2]*2 + A[3]*1 + A[4]*2
+rb = B[0]*6 + B[1]*3 + B[2]*2 + B[3]*1 + B[4]*2
+print(ra)
+print(rb)
