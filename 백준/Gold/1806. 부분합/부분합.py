8dfbe57d16afade77ac80e56299e3ff1b1959a03
Kwonmin Choi <109326426+Hello1Robot@users.noreply.github.com>
2023-02-19 01:23:39 +0900

    [Gold IV] Title: 부분합, Time: 140 ms, Memory: 42340 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.py
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -1,3 +1,6 @@
+# 실험포인트
+# min_lang이 1이 되면 프린트되고 종료시키면 더 빠를까?
+
 from sys import stdin; input = stdin.readline
 
 N, S = map(int,input().split())
@@ -9,6 +12,9 @@ while stt <= end and end <= N :
     
     if tot >= S:
         gap = end - stt
+        if gap == 1:
+            print(1)
+            exit()
         min_lang = min(min_lang, gap)
         tot -= nums[stt]
         stt += 1
@@ -20,4 +26,3 @@ while stt <= end and end <= N :
         end += 1
 
 [print(min_lang) if min_lang != 1e9 else print(0)]
-
