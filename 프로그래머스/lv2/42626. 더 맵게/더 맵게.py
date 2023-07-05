724087734075dea534f749fabf8d803048f34bc6
Kwonmin Choi <109326426+Hello1Robot@users.noreply.github.com>
2023-07-05 19:01:58 +0900

    [level 2] Title: 더 맵게, Time: 1680.91 ms, Memory: 51.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
@@ -0,0 +1,14 @@
+import heapq
+
+def solution(scoville, K):
+    answer = 0
+
+    heapq.heapify(scoville)
+
+    while scoville[0] < K:
+        if len(scoville) < 2:
+            return -1
+        new_food = heapq.heappop(scoville) + (heapq.heappop(scoville)*2)
+        heapq.heappush(scoville, new_food)
+        answer += 1
+    return answer
