2215f1b56e54bc60493eb674abd421fa7c9548b6
Kwonmin Choi <109326426+Hello1Robot@users.noreply.github.com>
2023-07-10 09:43:41 +0900

    [level 2] Title: 예상 대진표, Time: 0.21 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.java
@@ -0,0 +1,21 @@
+// A가 B보다 항상 크다는 가정이 없음 = 그냥 바꿔주는 게 좋을듯?
+
+class Solution
+{
+	public int solution(int n, int a, int b)
+	{
+		int answer = 0;
+		int new_a = Math.min(a,b);
+		int new_b = Math.max(a,b);
+		while(true){
+			answer+=1;
+			if (new_a%2 == 1 && new_b == new_a+1){
+				break;
+			}
+			new_a = (int)Math.ceil((double)new_a/2);
+			new_b = (int)Math.ceil((double)new_b/2);
+		}
+		
+		return answer;
+	}
+}
