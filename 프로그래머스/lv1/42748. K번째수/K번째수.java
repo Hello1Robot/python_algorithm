1909623dd18e2bb6bbd94b0887e024c169a5372c
Kwonmin Choi <109326426+Hello1Robot@users.noreply.github.com>
2023-08-22 10:07:48 +0900

    [level 1] Title: K번째수, Time: 1.38 ms, Memory: 79.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class Solution {
+	public int[] solution(int[] array, int[][] commands) {
+		int[] answer = new int[commands.length];
+		for (int i = 0; i < commands.length; i++) {
+			int[] command = commands[i];
+			int[] arr = Arrays.copyOfRange(array, command[0]-1, command[1]);
+			Arrays.sort(arr);
+			answer[i] = arr[command[2]-1];
+
+		}
+		return answer;
+	}
+}
