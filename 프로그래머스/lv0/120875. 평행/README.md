6572452d79a9a936ca43fa914c861919820c30a7
Kwonmin Choi <109326426+Hello1Robot@users.noreply.github.com>
2023-08-29 21:04:22 +0900

    [level 0] Title: 평행, Time: 0.02 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120875. 평행/README.md
+++ 프로그래머스/lv0/120875. 평행/README.md
@@ -1,10 +1,10 @@
 # [level 0] 평행 - 120875 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120875#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120875) 
 
 ### 성능 요약
 
-메모리: 77.5 MB, 시간: 0.01 ms
+메모리: 73.8 MB, 시간: 0.02 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120875. 평행/평행.java
+++ 프로그래머스/lv0/120875. 평행/평행.java
@@ -4,8 +4,12 @@ class Solution {
 	}
 
 	public int solution(int[][] dots) {
-		if(getSlope(dots[0][0], dots[0][1], dots[1][0], dots[1][1]) == getSlope(dots[2][0], dots[2][1], dots[3][0], dots[3][1]) || getSlope(dots[0][0], dots[0][1], dots[2][0], dots[2][1]) == getSlope(dots[1][0], dots[1][1], dots[3][0], dots[3][1]))
-			return 1;
+
+		for (int i = 1; i <= 2; i++) {
+			if(getSlope(dots[0][0], dots[0][1], dots[i][0], dots[i][1]) == getSlope(dots[3-i][0], dots[3-i][1], dots[3][0], dots[3][1]))
+				return 1;
+		}
+
 		return 0;
 	}
 }
